d4fb7df389644e0c726cfd257a0eee1fbf116bb6
Mauro Escobar <mauro@dribia.com>
2026-03-17 17:24:50 +0100

    :sparkles: use ty for type checking

--- tests/test_lock_parser.py
+++ tests/test_lock_parser.py
@@ -1,6 +1,7 @@
 """Test parsing of the uv.lock file and UVItems class."""
 
 import tomlkit
+from tomlkit import items as toml_items
 
 from sync_with_uv.main import UVItems
 from tests.helpers import LOCK_CONTENT
@@ -9,7 +10,7 @@ from tests.helpers import LOCK_CONTENT
 def test_uv_items_creation() -> None:
     """Test UVItems init."""
     content = tomlkit.loads(LOCK_CONTENT)
-    assert isinstance(content["package"], tomlkit.items.AoT)
+    assert isinstance(content["package"], toml_items.AoT)
     p = UVItems(content["package"])
     assert type(p._uv_lock) is dict
 
@@ -17,7 +18,7 @@ def test_uv_items_creation() -> None:
 def test_uv_items_metadata() -> None:
     """Test PreCommitRepo metadata (returned by UVItems.get_by_repo)."""
     content = tomlkit.loads(LOCK_CONTENT)
-    assert isinstance(content["package"], tomlkit.items.AoT)
+    assert isinstance(content["package"], toml_items.AoT)
     p = UVItems(content["package"])
     item = p.get_by_repo("https://github.com/pre-commit/mirrors-mypy")
     assert type(item) is dict
